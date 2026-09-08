70dfc77a41608a00e96d64c1a653576154d68191
ANTONIO CARLOS VIEIRA SANTOS <47786092803@info.local>
2026-09-08 10:52:00 -0300

    :construction: implementação de consumo de api. Seção 7 do guia consumindo uma api. Falta src/app/page.tsx

---
+++ consumo-api/src/types/produto.ts
@@ -0,0 +1,16 @@
+//Define estrutura dos dados --> Busca eles
+export type Produto = {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    category: string;
+    thumbnail: string;
+};
+
+export type RespostaPrdutos = { 
+    products: Produto[];
+    total: number;
+    skip: number;
+    limit: number;
+};
